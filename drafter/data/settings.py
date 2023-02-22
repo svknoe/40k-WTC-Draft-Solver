3ac82897f3bd89abaee0a14f82b91a6369dbe6cc
svknoe <33001939+svknoe@users.noreply.github.com>
2023-02-22 20:56:27 +0100

    Update settings.py

--- drafter/data/settings.py
+++ drafter/data/settings.py
@@ -4,11 +4,11 @@ show_friendly_strategy_suggestions = True
 show_enemy_strategy_suggestions = True
 
 # Configuration
-read_gamestates = True
-write_gamestates = True
+read_gamestates = False
+write_gamestates = False
 
-read_strategies = True
-write_strategies = True
+read_strategies = False
+write_strategies = False
 
 # Maximum number of attacker players considered by each team in each select attackers step.
 restrict_attackers = True

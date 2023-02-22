8d433e1a656adae487ef7f9f69a77d77f197244a
svknoe <33001939+svknoe@users.noreply.github.com>
2023-02-22 15:39:37 +0100

    move dictionary initialisation

--- drafter/app.py
+++ drafter/app.py
@@ -1,19 +1,9 @@
-import time  # standard libraries
-
-import drafter.common.utilities as utilities  # local source
-import drafter.common.teampermutation as teampermutation
-import drafter.data.settings as settings
-import drafter.data.matchinfo as matchinfo
-import drafter.solver.strategydictionaries as strategydictionaries
-import drafter.solver.gamestatedictionaries as gamestatedictionaries
+import drafter.data.initialisedictionaries as initialisedictionaries
 import drafter.solver.draft as draft
 
 
 def run():
-    enemy_team_prompt = "Select enemy team by entering the name of a folder in ..\\drafter\\resources\\matches:\n"
-    matchinfo.enemy_team_name = input(enemy_team_prompt)
-
-    initialise()
+    initialisedictionaries.initialise()
 
     while True:
         draft.play_draft()
@@ -21,36 +11,6 @@ def run():
         draft_again = prompt_draft_again()
         if not draft_again:
             break
-
-
-def initialise():
-    utilities.initialise_input_dictionary(matchinfo.pairing_dictionary, "pairing_matrix.txt", True)
-    utilities.initialise_input_dictionary(matchinfo.map_importance_dictionary, "map_importance_matrix.txt", False)
-
-    if (settings.restrict_attackers):
-        teampermutation.enable_restricted_attackers(settings.restricted_attackers_count)
-
-    t0 = time.time()
-    print()
-    print("Initialising gamestate dictionaries (This might take a few minutes):")
-    gamestatedictionaries.initialise_dictionaries(settings.read_gamestates, settings.write_gamestates)
-    print("time: {}s".format(round(time.time() - t0, 2)))
-
-    t0 = time.time()
-    print()
-    if settings.read_strategies:
-        print("Initialising strategy dictionaries (This might take a few minutes):")
-    else:
-        if settings.restrict_attackers and settings.restricted_attackers_count < 4:
-            print("Initialising strategy dictionaries (This might take a few minutes):")
-        elif settings.restrict_attackers and settings.restricted_attackers_count < 5:
-            print("Initialising strategy dictionaries (This might take an hour.):")
-        else:
-            long_runtime_warning = "Initialising strategy dictionaries (This might take many hours."
-            + " Enable restrict_attackers with restricted_attackers_count < 5 to reduce runtime.):"
-            print(long_runtime_warning)
-    strategydictionaries.initialise_dictionaries(settings.read_strategies, settings.write_strategies)
-    print("time: {}s".format(round(time.time() - t0, 2)))
 
 
 def prompt_draft_again():

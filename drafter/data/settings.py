c94d363a2a2b06d68f4c1f8c6bdf8e73e2062e1a
svknoe <33001939+svknoe@users.noreply.github.com>
2023-02-22 23:00:11 +0100

    Update settings.py

--- drafter/data/settings.py
+++ drafter/data/settings.py
@@ -2,13 +2,14 @@
 friendly_team_name = "Norway"
 show_friendly_strategy_suggestions = True
 show_enemy_strategy_suggestions = True
+invert_discard_attackers = True  # User to insert chosen enemy attacker instead of discarded enemy attacker.
 
 # Configuration
-read_gamestates = False
-write_gamestates = False
+read_gamestates = True
+write_gamestates = True
 
-read_strategies = False
-write_strategies = False
+read_strategies = True
+write_strategies = True
 
 # Maximum number of attacker players considered by each team in each select attackers step.
 restrict_attackers = True

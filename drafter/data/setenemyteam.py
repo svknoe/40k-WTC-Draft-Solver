1147bb09dae2b2f6230f60bfb7c3f1334999bd64
svknoe <33001939+svknoe@users.noreply.github.com>
2023-02-22 16:17:53 +0100

    Update setenemyteam.py

--- drafter/data/setenemyteam.py
+++ drafter/data/setenemyteam.py
@@ -1,5 +1,19 @@
-import drafter.data.matchinfo as matchinfo
+import os  # standard libraries
+from pathlib import Path
+
+import drafter.data.matchinfo as matchinfo  # local source
+
 
 def prompt_enemy_team():
+    drafter_path = Path(__file__).parent.parent
+    subfolder = "resources/matches"
+    matches_path = drafter_path / subfolder
+
+    teams = [x[0] for x in os.walk(matches_path)]
+
     enemy_team_prompt = "Select enemy team by entering the name of a folder in ..\\drafter\\resources\\matches:\n"
+
+    for team in teams:
+        print("    - " + team)
+
     matchinfo.enemy_team_name = input(enemy_team_prompt)

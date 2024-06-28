bb193f853240a9a2a96e751b28be4be55b1e0e69
Quentin Mennecart <qm@skalar.no>
2024-06-28 13:38:49 +0200

    chore: add better file finder and selector

--- drafter/data/setenemyteam.py
+++ drafter/data/setenemyteam.py
@@ -1,24 +1,21 @@
 import os  # standard libraries
 from pathlib import Path
+from InquirerPy import inquirer
 
-import drafter.data.matchinfo as matchinfo  # local source
+import drafter.data.matchinfo as match_info  # local source
 
 
 def prompt_enemy_team():
-    drafter_path = Path(__file__).parent.parent
-    subfolder = "resources/matches"
-    matches_path = drafter_path / subfolder
+    source_path = Path(__file__).parent.parent
+    matches_path = source_path / "resources/matches"
+    teams = os.listdir(matches_path)
+    if "matches" in teams: teams.remove("matches")
 
-    team_paths = [x[0] for x in os.walk(matches_path)]
-    teams = [team_path.split('\\')[-1] for team_path in team_paths]
-    teams.remove("matches")
+    match_info.enemy_team_name = inquirer.select(
+        message="Select enemy team:",
+        choices=teams,
+    ).execute()
+    confirm = inquirer.confirm(message="Confirm?").execute()
 
-    enemy_team_prompt = "Select enemy team by entering the name of a folder in ..\\drafter\\resources\\matches:"
-
-    for team in teams:
-        enemy_team_prompt += "\n    - " + team
-
-    enemy_team_prompt += '\n'
-
-    while matchinfo.enemy_team_name not in teams:
-        matchinfo.enemy_team_name = input(enemy_team_prompt)
+    if not confirm:
+        prompt_enemy_team()

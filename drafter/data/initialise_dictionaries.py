ac2cb29b3ee53d8e0447781b3c26e6ea9f5cc313
Quentin Mennecart <qm@skalar.no>
2024-06-29 17:47:04 +0200

    fix: correct local env setup do correct standards

--- drafter/data/initialise_dictionaries.py
+++ drafter/data/initialise_dictionaries.py
@@ -1,5 +1,6 @@
 import time  # standard libraries
 import sys
+from loguru import logger
 
 import drafter.common.utilities as utilities  # local source
 import drafter.common.team_permutation as team_permutation
@@ -17,13 +18,11 @@ def initialise():
         team_permutation.enable_restricted_attackers(settings.restricted_attackers_count)
 
     t0 = time.time()
-    print()
     print("Initialising gamestate dictionaries (This might take a few minutes):")
     game_state_dictionaries.initialise_dictionaries(settings.read_gamestates, settings.write_gamestates)
     print("time: {}s".format(round(time.time() - t0, 2)))
 
     t0 = time.time()
-    print()
     if settings.read_strategies:
         print("Initialising strategy dictionaries (This might take a few minutes):")
     else:
@@ -39,58 +38,53 @@ def initialise():
     print("time: {}s".format(round(time.time() - t0, 2)))
 
 
-def initialise_input_dictionary(empty_input_dictionary, filename, hard_crash, encoding={'--': -8, '-': -4, '0': 0, '+': 4, '++': 8}):
+def initialise_input_dictionary(empty_input_dictionary, filename, hard_crash):
+    encoding={'--': -8, '-': -4, '0': 0, '+': 4, '++': 8}
     path = utilities.get_path(filename)
 
+    # Read file
+    # ? It might not be needed to do that if we go for object oriented.
     try:
         with path.open(encoding="UTF-8") as f:
-            lines = f.read().splitlines()
+            lines = [line for line in f.read().splitlines() if line != ""]
     except:
         if hard_crash:
             raise SystemError("Missing file: {}".format(path))
         else:
-            print("Warning: Missing file: {}".format(path))
+            logger.error("Warning: Missing file: {path}")
             return
 
-    lines = [line for line in lines if line != ""]
+    # Init formatted line by setting the header
+    formattedLines = []
+    formattedLines.append(lines[0].split('|'))
 
-    tmpLines = []
-    for line in lines:
-        if line.count('|') > 0:
-            line = line.split('|')
-        else:
-            line = line.split()
-        tmpLines.append(line)
-    lines = tmpLines
+    formattedMatchups = []
 
-    for line in lines:
-        if (len(line) != len(lines[0])):
-            print("The following line has a number of elements not equal to the number of elements in the first line:")
-            print(line)
+    for index, line in enumerate(lines[1:], 1):
+        formattedLine: list[str] = line.split('|')
+
+        if (len(formattedLine) != len(formattedLines[0])):
+            logger.error("The following line has a number of elements not equal to the number of elements in the first line: \n{line}")
             sys.exit()
 
-    matchups = []
-    for i in range(2, len(lines)):
-        matchups.append(lines[i])
+        formattedLines.append(formattedLine)
 
-    tmpMatchups = []
-    for matchup in matchups:
-        tmpMatchup = []
-        for element in matchup:
-            try:
-                tmpMatchup.append(float(element))
-            except ValueError:
-                if element in encoding:
-                    tmpMatchup.append(encoding[element])
-                else:
-                    error_message = ("File {} contains unknown element {}. Use number values, specify encoding or use "
-                        + "default encoding: {}.").format(path, element, encoding)
-                    raise ValueError(error_message)
-        tmpMatchups.append(tmpMatchup)
-    matchups = tmpMatchups
+        if index > 1:
+            formattedMatchup = []
+            for element in formattedLine:
+                try:
+                    formattedMatchup.append(float(element))
+                except ValueError:
+                    if element in encoding:
+                        formattedMatchup.append(encoding[element])
+                    else:
+                        error_message = ("File {} contains unknown element {}. Use number values, specify encoding or use "
+                            + "default encoding: {}.").format(path, element, encoding)
+                        raise ValueError(error_message)
+            formattedMatchups.append(formattedMatchup)
 
-    friends = lines[0]
-    enemies = lines[1]
+    friends = formattedLines[0]
+    enemies = formattedLines[1]
 
     if settings.require_unique_names:
         for friend in friends:
@@ -102,7 +96,7 @@ def initialise_input_dictionary(empty_input_dictionary, filename, hard_crash, en
         row = {}
         enemyCounter = 0
         for enemy in enemies:
-            row[enemy] = matchups[friendCounter][enemyCounter]
+            row[enemy] = formattedMatchups[friendCounter][enemyCounter]
             enemyCounter += 1
         empty_input_dictionary[friend] = row
         friendCounter += 1
